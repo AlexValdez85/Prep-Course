80ce452f08366e854e3d97b0f2fe9aaf0aab0f2e
AlexValdez85 <alejandrovaldez802@gmail.com>
2022-09-30 12:45:45 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Hola otra vez";
+const nuevaString = "Hola otra denuvo otra vez";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
